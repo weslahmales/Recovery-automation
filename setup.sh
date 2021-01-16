e6ebc10f134106d9e9ac9864332af4ce1914d71e
Shéikh Adnan <manofuranium@gmail.com>
2021-01-16 10:53:39 +0530

    Added Cryptfs fix

--- setup.sh
+++ setup.sh
@@ -9,11 +9,22 @@ repo sync -j4 >> /dev/null
 echo "cloning dt/n"
 git clone $DT_LINK --depth=1 --single-branch $DT_PATH
 
-if [[ "$kernelisprebuilt" == "false" ]]; then
+if [[ "$KERNELISPREBUILT" == "false" ]]; then
        echo "using manual kernel compilation"
        git clone $KERNEL_LINK --depth=1 --single-branch $KERNEL_PATH
 else 
        echo "Kernel is prebuilt. Clone skipped."
+fi
+
+if [[ "$CRYPTFS_FIX" == "true" ]]; then
+       echo "fixing cryptfs"
+       cd work/vendor/qcom/opensource/commonsys/
+       rm -rf cryptfs_hw
+       rm Android.bp
+       wget https://raw.githubusercontent.com/ElytrA8/cryptfs_fixup/main/Android.bp
+       cd ../../../..
+else 
+       echo "Cryptfs fix not required. Skipped"
 fi
 
 echo "compiling recovery"
