7b62ca7165d060f1167c492ea3a8fa88a5e7202c
Shéikh Adnan <manofuranium@gmail.com>
2021-01-16 13:34:24 +0530

    env setup

---
+++ env.sh
@@ -0,0 +1,12 @@
+sudo apt install \
+     adb autoconf automake axel bc bison build-essential \
+     ccache clang cmake expat fastboot flex g++ \
+     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+     pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+     libxml-simple-perl apt-utils \
+     libncurses5 curl python-is-python3 -y
